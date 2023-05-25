639a2a1595380ab7fe1bb6def4f2579d2ca2c611
arc-ch <134518231+arc-ch@users.noreply.github.com>
2023-05-25 10:57:03 +0530

    Update largest3elements.c

--- largest3elements.c
+++ largest3elements.c
@@ -37,3 +37,39 @@ int main() {
 
     return 0;
 }
+
+
+// #include <stdio.h>    
+// int main() {
+//     int size;
+//     printf("Enter the size of the array: ");
+//     scanf("%d", &size);
+
+//     int arr[size];
+//     printf("Enter %d elements of the array: ", size);
+//     for (int i = 0; i < size; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     int first=0;
+//     int second =0;
+//     int third=0;
+//     for (int i = 0; i < size; i++) {
+//         if (arr[i] > first) {
+//             third = second;
+//             second = first;
+//             first = arr[i];
+//         } else if (arr[i] > second && arr[i] != first) {
+//             third = second;
+//             second = arr[i];
+//         } else if (arr[i] > third && arr[i] != second && arr[i] != first) {
+//             third = arr[i];
+//         }
+//     }
+//     printf("First highest element: %d\n", first);
+//     printf("Second highest element: %d\n", second);
+//     printf("Third highest element: %d\n", third);
+
+//     return 0;
+// }
+
